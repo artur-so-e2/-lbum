efd52418f2782fe5fe5716a00d7725202684d03f
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2026-08-13 15:32:36 -0300

    Adição de função sobre álbuns

--- desenvolviento.py
+++ desenvolviento.py
@@ -17,15 +17,32 @@ def listar_figuras(figuras):
             print(f"NOME: {figura['nome']}    ID: {figura['id']}    ID DO ÁLBUM: {figura['id_album']}    OBTIDO: Sim")
 
 def comprar_album(albuns):
-    for album in albuns:
-        if album['obtido'] == False:
-            print(f"NOME: {album['nome']}    PREÇO: R${album['preco']}    ID: {album['id']}    OBTIDO: Não")
+    while(True):
+        resultado = None
+        listar_album(albuns)
 
-        elif album['obtido'] == True:
-            print(f"NOME: {album['nome']}    PREÇO: R${album['preco']}    ID: {album['id']}    OBTIDO: Sim")
+        opcao = input("Digite qual o id do álbum você deseja comprar: ")
 
-def comprar_pacotes(pacotes):
-    print
+        for album in albuns:
+            if opcao == album['id']:
+                album['obtido'] = True
+                resultado = album
+                print("Compra efetuada com sucesso!")
+        if not resultado:
+            print("Erro, tente novamente.")
+            continue
+        else:
+            opcao2 = input("Digite se quer continuar comprando álbuns (S/N): ").upper()
+            if opcao2 != 'S':
+                break
+
+def comprar_pacotes(figuras, pacotes):
+    for figura in figuras:
+        if figura['obtido'] == False:
+            print(f"NOME: {figura['nome']}    ID: {figura['id']}    ID DO ÁLBUM: {figura['id_album']}    OBTIDO: Não")
+
+        elif figura['obtido'] == True:
+            print(f"NOME: {figura['nome']}    ID: {figura['id']}    ID DO ÁLBUM: {figura['id_album']}    OBTIDO: Sim")
 
 def abrir_pacotes(pacotes):
     print
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from desenvolviento import listar_album, listar_figuras, comprar_album, comprar_pacotes, abrir_pacotes
 
-albuns = []
+albuns = [{'nome': 'Abcdário', 'preco': 25, 'id': '67', 'obtido': False},]
 
-figuras = []
+figuras = [{'nome': 'A', 'id': '1', 'id_album': '67', 'obtido': False},
+           {'nome': 'B', 'id': '2', 'id_album': '67', 'obtido': False},
+           {'nome': 'C', 'id': '3', 'id_album': '67', 'obtido': False},
+           {'nome': 'D', 'id': '4', 'id_album': '67', 'obtido': False},
+           {'nome': 'E', 'id': '5', 'id_album': '67', 'obtido': False},
+           {'nome': 'F', 'id': '6', 'id_album': '67', 'obtido': False},
+           {'nome': 'G', 'id': '7', 'id_album': '67', 'obtido': False},
+           {'nome': 'H', 'id': '8', 'id_album': '67', 'obtido': False},]
 
 pacotes = []
 
@@ -18,19 +25,19 @@ while(True):
     escolha = input('Digite a opção que deseja fazer: ')
 
     if escolha == '1':
-        listar_album()
+        listar_album(albuns)
 
     elif escolha == '2':
-        listar_figuras()
+        listar_figuras(figuras)
 
     elif escolha == '3':
-        comprar_album()
+        comprar_album(albuns)
 
     elif escolha == '4':
-        comprar_pacotes()
+        comprar_pacotes(pacotes)
 
     elif escolha == '5':
-        abrir_pacotes()
+        abrir_pacotes(pacotes)
 
     elif escolha == '6':
         break
